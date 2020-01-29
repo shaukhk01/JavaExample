f80514f05640238b10b7feb602f185fc083d025d
shaukhk01 <shaukhk01@gmail.com>
2020-01-29 08:19:13 +0530

    java90

---
+++ p90.java
@@ -0,0 +1,11 @@
+class SimpleStaticExample
+{
+    static void myMethod()
+    {
+        System.out.println("mySaticMethod");
+    }
+    public static void main(String[]args)
+    {
+        myMethod();
+    }
+}
