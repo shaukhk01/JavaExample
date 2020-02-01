b0594bf5828fd5a2bb6d0cbc2b4e5e7422be6ec5
shaukhk01 <shaukhk01@gmail.com>
2020-02-01 15:16:55 +0530

    length of array

---
+++ BoxDemo16.java
@@ -0,0 +1,14 @@
+class Length
+{
+    public static void main(String[]args)
+    {
+        int arr1[] = new int[10];
+        int arr2[] = {3,5,7,1,9,8,2,76};
+        int arr3[] = {4,3,3,1};
+
+        System.out.println("length of a1: " + arr1.length);
+        System.out.println("length of a2: " + arr2.length);
+        System.out.println("length of a3: " + arr3.length);
+    }
+}
+
