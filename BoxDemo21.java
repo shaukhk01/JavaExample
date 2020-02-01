af9324e70e1738da0119d304d984c5ff461a88b6
shaukhk01 <shaukhk01@gmail.com>
2020-02-01 19:14:44 +0530

    StringArray

---
+++ BoxDemo21.java
@@ -0,0 +1,12 @@
+class StringDemo
+{
+    public static void main(String[]args)
+    {
+        String str[] = {"one","two","three","four","five"};
+
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("["+i+"]: " + str[i]);
+        }
+    }
+}
