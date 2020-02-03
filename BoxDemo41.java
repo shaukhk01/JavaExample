2578271699f4567bfe014808896d1e198732ac28
shaukhk01 <shaukhk01@gmail.com>
2020-02-03 21:44:10 +0530

    Dynamic metho dispatch

---
+++ BoxDemo41.java
@@ -0,0 +1,43 @@
+class A 
+{
+    void callme()
+    {
+        System.out.println("Inside A's callme method");
+    }
+}
+class B extends A
+{
+    //override
+    void callme()
+    {
+        System.out.println("Inside B's callme method");
+    }
+}
+class C extends A
+{
+    //override
+    void callme()
+    {
+        System.out.println("Inside C's callme method");
+    }
+}
+class Execute
+{
+    public static void main(String[]args)
+        {
+            A a = new A();
+            B b = new B();
+            C c = new C();
+            
+            A r;//obtain a reference of type A
+            r = a;//r refer to object A
+            r.callme();
+            r = b;
+            r.callme();
+            r = c;
+            r.callme();
+
+        }
+}
+
+
