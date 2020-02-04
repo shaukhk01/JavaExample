cf14f0ac1ae3055892af0f99a3725658d136ef11
shaukhk01 <shaukhk01@gmail.com>
2020-02-04 12:29:43 +0530

    childPackage

---
+++ DemoBox45.java
@@ -0,0 +1,10 @@
+import Mypack.BoxDemo45;
+
+class Execute
+{
+    public static void main(String[]args)
+    {
+        BoxDemo45 box = new BoxDemo45("annie",220.0);/*if BoxDemo45 is not public then not able to 
+        box.show();                                   * access from another class 
+    }                                                 */
+}
