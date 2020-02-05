757eb30daf70bb40e3eccc6d557509f2aa4d7080
shaukhk01 <shaukhk01@gmail.com>
2020-02-05 10:01:50 +0530

    A-Z printing

---
+++ BoxDemo54.java
@@ -0,0 +1,14 @@
+class CharDemo
+{
+    public static void main(String[]args)
+    {
+        char cha1,cha2;
+        cha1 = 'A';
+        cha2 = cha1;
+        System.out.println(cha1);
+        for(int i=0;i<25;i++)
+        {
+        System.out.println(++cha2);
+    }
+}
+}
