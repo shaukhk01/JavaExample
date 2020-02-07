46b982bfb5ace29240ecc46582069c02597ca7d3
shaukhk01 <shaukhk01@gmail.com>
2020-02-07 22:46:28 +0530

    eachIndexObjectcreteandcallmethofofeachobject

---
+++ BoxDemo76.java
@@ -0,0 +1,39 @@
+class MonsterTestDrive
+{
+    public static void main(String[]args)
+    {
+        Monster[] ma = new Monster[3];
+        ma[0] = new Vampire();
+        ma[1] = new Dragon();
+        ma[2] = new Monster();
+
+        for(int x = 0;x<3;x++)
+        {
+            ma[x].frighten(x);
+        }
+    }
+}
+class Monster
+{
+    boolean  frighten(int d)
+    {
+        System.out.println("arrgh");
+        return true;
+    }
+}
+class Vampire extends Monster
+{
+    boolean frighten(int x)
+    {
+        System.out.println("a bite?");
+        return true;
+    }
+}
+class Dragon extends Monster
+{
+    boolean frighten(int degree)
+    {
+        System.out.println("breath fire");
+        return true;
+    }
+}
