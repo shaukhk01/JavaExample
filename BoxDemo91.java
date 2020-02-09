64ea6706b50f4eb4f4d862384b99aef0aa99069d
shaukhk01 <shaukhk01@gmail.com>
2020-02-09 11:19:28 +0530

    inputmethod()

---
+++ BoxDemo91.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class InputTest
+{
+    public static void main(String[]args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name? ");
+       String name = in.nextLine();
+
+       System.out.println("How old are you? ");
+       int age = in.nextInt();
+
+       System.out.println("Hello, "+name +".Next year,you ll be"+age);
+    }
+}
