3cd6f811040fcf3904e3799bc89ade8b56b11d1e
shaukhk01 <shaukhk01@gmail.com>
2020-02-15 17:54:10 +0530

    polymorphismBestExampleRealTime                                   Delivey Boys deliverd  Letter,Devery Boy also deliverd PIzza

---
+++ Polymorphism.java
@@ -0,0 +1,37 @@
+package com.journaldev;
+import java.util.Random;
+
+class DeliveryBoy
+{
+    void deliver()
+    {
+        System.out.println("Delivering Item");
+    }
+    public static void main(String[]args)
+    {
+        DeliveryBoy db=  getDeliveryBoy();
+        db.deliver();
+    }
+    private static DeliveryBoy getDeliveryBoy()
+    {
+        Random rand = new Random();
+        int num = rand.nextInt(10);
+        return num %2 == 0 ? new PizzaDeliveryBoy():new Postman();
+    }
+}
+class PizzaDeliveryBoy extends DeliveryBoy
+{
+   // @override
+    void deliver()
+    {
+        System.out.println("Delivering Pizza");
+    }
+}
+class Postman extends DeliveryBoy
+{
+//    @override
+    void deliver()
+    {
+        System.out.println("Delivering Letters");
+    }
+}
