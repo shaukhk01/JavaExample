dfe7afdc9f1740d1767fdb2a532541f787b4f27d
shaukhk01 <shaukhk01@gmail.com>
2020-02-17 11:52:51 +0530

    AbstractionInterface part of

--- Execute.java
+++ Execute.java
@@ -7,6 +7,9 @@ public class Execute
         ai.turnOnCar();
         ai.turnOffCar();
         System.out.println(ai.getCarType());
+        /*here you can see in CLIENT program only know about Car  and car function
+         *car provides The internal implementation details are hidden from client program.
+         */
     }
 }
 
